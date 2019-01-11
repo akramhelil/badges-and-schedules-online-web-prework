25ec05baabbb42ca2ea1d47856c6c506edf98f7f
Akram Helil <akramhelil@gmail.com>
2019-01-11 21:55:55 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -2,5 +2,10 @@
 attendence = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
 
 def badge_maker(name)
-  puts "Hello, my name is#{name} ."
+"Hello, my name is#{name} ."
+end
+
+def batch_badge_creator(attendence)
+
+
 end
